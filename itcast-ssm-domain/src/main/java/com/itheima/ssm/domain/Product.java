711b0abddc3dad6119f80f853113956f8f364a17
Guo YanJiang <296951801@qq.com>
2019-10-31 17:16:29 +0800

    Initial commit

---
+++ itcast-ssm-domain/src/main/java/com/itheima/ssm/domain/Product.java
@@ -0,0 +1,141 @@
+package com.itheima.ssm.domain;
+
+import com.itheima.ssm.utils.DateUtils;
+
+import java.util.Date;
+
+public class Product {
+
+    private String id;//主键
+    private String productNum;//产品编号 唯一
+    private String productName;//产品名称
+    private String cityName;//城市
+    private Date departureTime;//出发时间
+    private String departureTimeStr;
+    private Double productPrice;//产品价格
+    private String productDesc;//产品描述
+    private Integer productStatus;//产品状态 0 关闭 1 开启
+    private String productStatusStr;
+
+    public Product() {
+    }
+
+    public Product(String id, String productNum, String productName, String cityName, Date departureTime, String departureTimeStr, Double productPrice, String productDesc, Integer productStatus, String productStatusStr) {
+        this.id = id;
+        this.productNum = productNum;
+        this.productName = productName;
+        this.cityName = cityName;
+        this.departureTime = departureTime;
+        this.departureTimeStr = departureTimeStr;
+        this.productPrice = productPrice;
+        this.productDesc = productDesc;
+        this.productStatus = productStatus;
+        this.productStatusStr = productStatusStr;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductNum() {
+        return productNum;
+    }
+
+    public void setProductNum(String productNum) {
+        this.productNum = productNum;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getDepartureTimeStr() {
+        if (departureTime != null){
+            departureTimeStr = DateUtils.DateToString(departureTime);
+        }
+        return departureTimeStr;
+    }
+
+    public void setDepartureTimeStr(String departureTimeStr) {
+        this.departureTimeStr = departureTimeStr;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public Integer getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(Integer productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public String getProductStatusStr() {
+        //产品状态 0 关闭 1 开启
+        if (productStatus == 0){
+            productStatusStr = "关闭";
+        }
+        if (productStatus == 1){
+            productStatusStr = "开启";
+        }
+        return productStatusStr;
+    }
+
+    public void setProductStatusStr(String productStatusStr) {
+        this.productStatusStr = productStatusStr;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", productNum='" + productNum + '\'' +
+                ", productName='" + productName + '\'' +
+                ", cityName='" + cityName + '\'' +
+                ", departureTime=" + departureTime +
+                ", departureTimeStr='" + departureTimeStr + '\'' +
+                ", productPrice=" + productPrice +
+                ", productDesc='" + productDesc + '\'' +
+                ", productStatus=" + productStatus +
+                ", productStatusStr='" + productStatusStr + '\'' +
+                '}';
+    }
+}
---
+++ itcast-ssm-utils/src/main/java/com/itheima/ssm/utils/DateUtils.java
@@ -0,0 +1,13 @@
+package com.itheima.ssm.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtils {
+
+    public static String DateToString(Date date){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String s = sdf.format(date);
+        return s;
+    }
+}
